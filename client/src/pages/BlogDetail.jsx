1ea6aa4eb511633dc0d347b2229020bb59471167
Lê Trọng Tuyên <85683857+tuyenlt@users.noreply.github.com>
2025-04-19 09:23:58 +0000

    cm

--- client/src/pages/BlogDetail.jsx
+++ client/src/pages/BlogDetail.jsx
@@ -1,30 +1,44 @@
-import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
 
 const BlogDetail = () => {
-    const location = useLocation()
-    const post = location.state?.post
+  const { id } = useParams();
+  const [post, setPost] = useState();
 
-    if (!post) {
-        return (
-            <div className="p-8 text-center">
-                <h2 className="text-2xl text-red-600">Post not found</h2>
-                <Link to="/blog" className="text-blue-500 hover:underline">Back to Blog</Link>
-            </div>
-        );
-    }
+  useEffect(() => {
+    const fetchPostDetail = async () => {
+      const response = await fetch(`https://localhost:8080/blog/${id}`);
+      const postDetail = await response.json();
+      setPost(postDetail);
+    };
+    fetchPostDetail();
+  }, []);
 
-    return (
+  return (
+    <div>
+      {post ? (
         <div className="max-w-3xl mx-auto p-6 bg-white shadow-md rounded mt-8">
-            <h1 className="text-3xl font-bold text-blue-700 mb-2">{post.title}</h1>
-            <p className="text-sm text-gray-500 mb-4">By {post.author} • {post.date}</p>
-            <p className="whitespace-pre-line text-gray-800 leading-relaxed">
-                {post.content}
-            </p>
-            <Link to="/blog" className="inline-block mt-6 text-blue-600 hover:underline">
-                ← Back to Blog
-            </Link>
+          <h1 className="text-3xl font-bold text-blue-700 mb-2">
+            {post.title}
+          </h1>
+          <p className="text-sm text-gray-500 mb-4">
+            By {post.author} • {post.date}
+          </p>
+          <p className="whitespace-pre-line text-gray-800 leading-relaxed">
+            {post.content}
+          </p>
+          <Link
+            to="/blog"
+            className="inline-block mt-6 text-blue-600 hover:underline"
+          >
+            ← Back to Blog
+          </Link>
         </div>
-    );
+      ) : (
+        "loading"
+      )}
+    </div>
+  );
 };
 
 export default BlogDetail;
